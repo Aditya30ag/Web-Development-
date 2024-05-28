086765427fc610137d8196b3dd38809222cb0a65
Aditya Agrawal <162010355+Aditya30ag@users.noreply.github.com>
2024-05-28 16:01:56 +0530

    Only practice for Javascript.

---
+++ Game/Rock paper scissors/second.js
@@ -0,0 +1,64 @@
+let user = 0;
+let comp = 0;
+
+const choice1 = document.querySelector(".button1");
+const choice2 = document.querySelector(".button2");
+const choice3 = document.querySelector(".button3");
+
+const msg=document.querySelector(".hello");
+
+const h=document.querySelector(".user");
+const j=document.querySelector(".comp");
+
+const gencompchoice=()=>{
+  const option=["button1","button2","button3"];
+  const random=Math.floor(Math.random()*3);
+  return option[random];
+}
+
+const showwinner=(userwin)=>{
+  if (userwin){
+    user++;
+    h.innerText=user;
+    msg.innerText="You win !";
+    msg.style.backgroundColor="green";
+  }else{
+    comp++;
+    j.innerText=comp;
+    msg.innerText="You lose.";
+    msg.style.backgroundColor="red";
+  }
+}
+
+const playgame=(a)=>{
+    const compchoice=gencompchoice();
+
+    if (a===compchoice){
+      msg.innerText="Game was Draw.Play again";
+      msg.style.backgroundColor="#211845";
+    }else{
+      let userwin=true;
+      if (a==="button1"){
+        userwin=compchoice==="button3"?false:true;
+      }else if (a==="button2"){
+        userwin=compchoice==="button1"?false:true;
+      }else{
+        userwin=compchoice==="button2"?false:true;
+      }
+      showwinner(userwin);
+    }
+};
+
+
+choice1.addEventListener("click", () => {
+  const a=choice1.getAttribute("class");
+  playgame(a);
+});
+choice2.addEventListener("click", () => {
+  const a=choice2.getAttribute("class");
+  playgame(a);
+});
+choice3.addEventListener("click", () => {
+  const a=choice2.getAttribute("class");
+  playgame(a);
+});
